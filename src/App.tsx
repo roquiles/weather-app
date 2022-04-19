170c0c480adfc304086ee70b3f171b269ed76ae1
roquiles <roberta_btg@hotmail.com>
2022-04-19 10:58:45 -0300

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { WeatherCard } from "./components/WeatherCard";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
       <WeatherCard />
+      <GlobalStyle />
     </div>
   );
 }
