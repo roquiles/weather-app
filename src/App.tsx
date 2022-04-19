1f816d724c5aecc8041f88a27e70f75612c028ba
roquiles <roberta_btg@hotmail.com>
2022-04-19 10:47:14 -0300

    Add WeatherCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { WeatherCard } from "./components/WeatherCard";
+
 export function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <WeatherCard />
     </div>
   );
 }
